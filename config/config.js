ec1ec37126e669d0deea10935ef10b7d53ed059a
B K O <97314386+NextRaphi@users.noreply.github.com>
2023-10-22 03:17:13 +0200

    Updated 22.10.2023 | 03:17

--- config/config.js
+++ config/config.js
@@ -1,31 +1,31 @@
 module.exports ={
 bot_start: {
-    token: 'MTE2Mzk0NzMwNTYxMTgzNzQ0Mg.G14tM4.zAKB3MJ-UC1naCXPCWiPEnur7w1XZKx6v43YcQ',
-    client_Id: '1163947305611837442',
-    guildId: '1161227476240977921',
+    token: 'Bot Token Here',
+    client_Id: 'Client ID Here',
+    guildId: 'Guild ID Here',
 },
 
 Moderation: {
-    muteroleid: '1165389366881959966',
+    muteroleid: '',
 },
 
 Permissions: {
-    staffid: '1161235528394477619',
-    adminid: '1161229693987590154',
+    staffid: '',
+    adminid: '',
 },
 
 TicketSystem: {
-    ticketlogs: '1163442314597896253',
+    ticketlogs: '',
 
     TicketCategory: {
-        support: '1163243877835231232',
-        application: '1163243877835231232',
-        productpurchase: '1163243877835231232',
+        support: '',
+        application: '',
+        productpurchase: '',
     }
 },
 
 VerifySystem: {
-    verifyrole: '1161229604791525426',
+    verifyrole: '',
 },
 
 DiscountCodes: {
